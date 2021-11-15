c5df32a852ecab9bc54cf75725f17d9785572380
gongli.qiu <gongli.qiu@dsky.com>
2021-11-15 15:00:54 +0800

    Android打包脚本

--- build-Android.bat
+++ build-Android.bat
@@ -1,9 +1,9 @@
-mkdir TempBuild
-cd TempBuild
+mkdir TempBuildAndroid
+cd TempBuildAndroid
 
-set NDK_ROOT=C:/Users/gongli.qiu/AppData/Local/Android/Sdk/ndk/21.1.6352462
+set NDK_ROOT=C:\Users\gongli.qiu\Desktop\android-ndk-r14b
 set PB_SOURCE=%~dp0
 
-cmake -G "MinGW Makefiles" -DPLATFORM=Android -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DCMAKE_MAKE_PROGRAM=%NDK_ROOT%/prebuilt/windows-x86_64/bin/make.exe -DANDROID_STL=c++_shared  %PB_SOURCE%
+cmake -G "MinGW Makefiles" -DCMAKE_CXX_FLAGS_RELWITHDEBINFO= -DANDROID_ABI=x86_64 -DPLATFORM=Android -DCMAKE_TOOLCHAIN_FILE=%NDK_ROOT%/build/cmake/android.toolchain.cmake -DCMAKE_MAKE_PROGRAM=%NDK_ROOT%/prebuilt/windows-x86_64/bin/make.exe -DANDROID_STL=c++_shared -DANDROID_TOOLCHAIN=gcc %PB_SOURCE%
 
-cmake --build . --target UE4-Protobuf
+cmake --build . --target UE4-Protobuf 
